93692382097ca27aebf901e2fb145a43eeb187ee
MirzayevDeveloper <MirzayevDeveloper1@gamil.com>
2023-03-06 21:50:03 +0500

    CODE RUB: Person1 Model Renamed to Person

--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -1,16 +1,14 @@
 ﻿// Welcome
 
-using Bogus;
-using Domain.Enums;
-using Infrastructure.Models.Person;
 using Infrastructure.Services;
-
-Faker faker = new Faker();
+using Tynamix.ObjectFiller;
 
 bool isActive = true;
 
 while (isActive)
 {
+    Console.Clear();
+
     Console.Write("1.Add Person\n2.Update Person\n3.Read Person\n" +
         "4.Delete Person\n5.Top 5 people better distance\n6.GetAll person " +
         "by distance discanding\n7.GetAll person by address\nChoose option: ");
@@ -22,7 +20,7 @@ while (isActive)
     switch (choice)
     {
         case 1:
-                RandomAdd();
+            RandomAdd();
             break;
         case 2:
             break;
@@ -39,25 +37,22 @@ while (isActive)
     }
 }
 
-
 void RandomAdd()
 {
-    int point = faker.Random.Int(0, 2);
-    Gender gender = point == 0 ? Gender.Male : Gender.Female;
 
-    Person1 person = new()
-    {
-        PassportId = faker.Random.Guid().ToString(),
-        FullName = faker.Name.FullName(),
-        Weight = faker.Random.Double(40, 200),
-        Gender = gender,
-        Age = faker.Random.Int(18, 100),
-        Id = Guid.NewGuid(),
-        OverallWalkDistance = 0,
-        WalkDistances = new List<Guid>() { }
-    };
+
 
     PersonService personService = new PersonService();
     var task = personService.AddPersonAsync(person);
 
+}
+
+Filler<Person> CreateLanguageFiller(DateTimeOffset dates)
+{
+    var filler = new Filler<Language>();
+
+    filler.Setup()
+        .OnType<DateTimeOffset>().Use(dates);
+
+    return filler;
 }
